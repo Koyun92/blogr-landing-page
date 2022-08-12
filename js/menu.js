94e956059612c09460c886c7bf42f2475ce7de71
Borys <borys.baca@gmail.com>
2022-08-12 11:32:57 +0200

    mobile nav js and css added part 2

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,25 @@
-const listItem = document.querySelectorAll('.nav__main-element');
-const hamburgerOpen = document.querySelectorAll('.header__menu-img');
+const burgerToggle = () => {
+    const hamburgerIcon = document.querySelectorAll('.header__burger-icon');
+    const mobileNav = document.querySelector('.nav-mobile__list');
+    hamburgerIcon.forEach(item => {
 
+        item.addEventListener('click', () => {
+            mobileNav.classList.toggle('nav-mobile__list--flex');
+            for (let i = 0; i <= hamburgerIcon.length - 1; i++) {
 
+                hamburgerIcon[i].classList.toggle('active');
+            }
+        })
+    })
+}
 
 
 const dropdownList = () => {
+    const listItem = document.querySelectorAll('.nav__dropdown-button');
     listItem.forEach((item) => {
         item.addEventListener('click', () => {
-            item.nextElementSibling.classList.toggle('nav__dropdown-list--active')
+            item.nextElementSibling.classList.toggle('nav__dropdown-list--active');
+            item.lastChild.classList.toggle('nav__icon--rotate');
         })
 
     })
@@ -15,5 +27,4 @@ const dropdownList = () => {
 
 
 dropdownList();
-menuToggle(hamburgerOpen);
-menuToggle(hamburgerClose)
+burgerToggle();
