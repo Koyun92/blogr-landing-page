6662670ad6e9de4d7f1768e4e8014a7334da0d57
Borys <borys.baca@gmail.com>
2022-08-17 10:14:51 +0200

    nav desktop almost styled

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,25 @@
 const burgerToggle = () => {
-    const hamburgerIcon = document.querySelectorAll('.header__burger-icon');
-    const mobileNav = document.querySelector('.nav-mobile__list');
-    hamburgerIcon.forEach(item => {
-
-        item.addEventListener('click', () => {
-            mobileNav.classList.toggle('nav-mobile__list--flex');
-            for (let i = 0; i <= hamburgerIcon.length - 1; i++) {
-
-                hamburgerIcon[i].classList.toggle('active');
-            }
-        })
-    })
-}
-
+  const hamburgerIcon = document.querySelectorAll(".header__burger-icon");
+  const mobileNav = document.querySelector(".nav-mobile__list");
+    hamburgerIcon.forEach((item) => {
+        item.addEventListener("click", () => {
+      mobileNav.classList.toggle("nav-mobile__list--flex");
+      for (let i = 0; i <= hamburgerIcon.length - 1; i++) {
+        hamburgerIcon[i].classList.toggle("active");
+      }
+    });
+  });
+};
 
 const dropdownList = () => {
-    const listItem = document.querySelectorAll('.nav__dropdown-button');
-    listItem.forEach((item) => {
-        item.addEventListener('click', () => {
-            item.nextElementSibling.classList.toggle('nav__dropdown-list--active');
-            item.lastChild.classList.toggle('nav__icon--rotate');
-        })
-
-    })
-}
-
+  const listItem = document.querySelectorAll(".nav-dropdown-button");
+  listItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.nextElementSibling.classList.toggle("nav-dropdown-list--active");
+      item.lastChild.classList.toggle("nav-icon--rotate");
+    });
+  });
+};
 
 dropdownList();
 burgerToggle();
